2d53f86d76e83ec8106edbe76fa20006bf6428dc
Houl <102855377+ItsHoul@users.noreply.github.com>
2022-07-29 21:30:20 +0300

    Create docs.js

---
+++ assets/scripts/docs.js
@@ -0,0 +1,41 @@
+function langRU() {
+		document.getElementById("page-title").innerHTML = "Документация";
+        document.getElementById("page-desc").innerHTML = "Немного полезной информации о командах";
+        document.getElementById("goback-btn").innerHTML = "Назад";
+        
+        document.getElementById("cc-title").innerHTML = "Команды холста";
+        document.getElementById("cc-desc").innerHTML = "Как использовать /place и /view";
+        
+        document.getElementById("pc-title").innerHTML = "Команды профиля";
+        document.getElementById("pc-desc").innerHTML = '"Как сменить баннер, а?"';
+        
+        
+        document.getElementById("ac-title").innerHTML = "Админские команды";
+        document.getElementById("ac-desc").innerHTML = "Так, кто поменял язык бота?";
+        
+        document.getElementById("tmh1").innerHTML = "Посмотреть";
+        document.getElementById("tmh2").innerHTML = "Посмотреть";
+        document.getElementById("tmh3").innerHTML = "Посмотреть";
+        document.getElementById("by").innerHTML = "Сделано с 🍞 Houl#1350";
+	}
+
+  function langEN() {
+  document.getElementById("page-title").innerHTML = "Docs";
+      document.getElementById("page-desc").innerHTML = "Some helpful information about commands";
+      document.getElementById("goback-btn").innerHTML = "Go back";
+
+      document.getElementById("cc-title").innerHTML = "Canvas commands";
+      document.getElementById("cc-desc").innerHTML = "How to use /place and /view";
+
+      document.getElementById("pc-title").innerHTML = "Profile commands";
+      document.getElementById("pc-desc").innerHTML = '"How do i change my banner, huh?"';
+
+
+      document.getElementById("ac-title").innerHTML = "Admin commands";
+      document.getElementById("ac-desc").innerHTML = "Who changed bot's language to russian?";
+
+      document.getElementById("tmh1").innerHTML = "Take me there";
+      document.getElementById("tmh2").innerHTML = "Take me there";
+      document.getElementById("tmh3").innerHTML = "Take me there";
+      document.getElementById("by").innerHTML = "Made with 🍞 by Houl#1350";
+}
